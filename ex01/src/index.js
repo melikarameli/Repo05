a433601bb30d2a3f49472ea70d02920035d63cdc
melikarameli <melikahatunic2001@gmail.com>
2022-04-01 09:47:20 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Melika";
+    var myLastName = "Hatunic";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
