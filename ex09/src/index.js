289ac505dd414eaf5a278db9d9db862f1038ebae
melikarameli <melikahatunic2001@gmail.com>
2022-04-01 12:46:12 +0200

    Added ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Doe".length;
+    var lastNameLength = lastName;
+    // Only change code above this line
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
